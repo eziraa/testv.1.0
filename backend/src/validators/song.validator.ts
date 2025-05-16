ae61261870f28bb9bc7fc3ee5c674a3a39bce6b3
eziraa <ezratgab@gmail.com>
2025-05-16 15:20:38 +0300

    feat: create zod song validation schema

---
+++ backend/src/validators/song.validator.ts
@@ -0,0 +1,28 @@
+// src/validators/songValidator.ts
+import { z } from 'zod';
+import mongoose from 'mongoose';
+
+export const SongSchemaZod = z.object({
+  title: z.string().min(1, "Title is required"),
+  artist: z.string().refine(val => mongoose.Types.ObjectId.isValid(val), {
+    message: "Invalid artist ID"
+  }),
+  album: z.string().optional().refine(
+    val => val === undefined || mongoose.Types.ObjectId.isValid(val),
+    { message: "Invalid album ID" }
+  ),
+  genre: z.string().optional(),
+  audioUrl: z.string().url("Invalid audio URL"),
+  releaseDate: z.union([z.string().datetime(), z.date()]).optional()
+});
+export type SongInput = z.infer<typeof SongSchemaZod>;
+export const SongUpdateSchemaZod = SongSchemaZod.partial().extend({
+  artist: z.string().refine(val => mongoose.Types.ObjectId.isValid(val), {
+    message: "Invalid artist ID"
+  }),
+  album: z.string().optional().refine(
+    val => val === undefined || mongoose.Types.ObjectId.isValid(val),
+    { message: "Invalid album ID" }
+  ),
+});
+export type SongUpdateInput = z.infer<typeof SongUpdateSchemaZod>;
