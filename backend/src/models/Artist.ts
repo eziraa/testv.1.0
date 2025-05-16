b5269166e01838eb0c80e544b57e3fb72d0e3861
eziraa <ezratgab@gmail.com>
2025-05-16 21:10:55 +0300

    update: artist nidel to add profile pricture

--- backend/src/models/Artist.ts
+++ backend/src/models/Artist.ts
@@ -3,11 +3,13 @@
   export interface IArtist extends Document {
     name: string;
     bio?: string;
+    profilePicture?: string;
   }
   
   const ArtistSchema = new Schema<IArtist>({
     name: { type: String, required: true },
-    bio: { type: String }
+    bio: { type: String },
+    profilePicture: { type: String },
   });
 
   export default mongoose.model<IArtist>('Artist', ArtistSchema);
