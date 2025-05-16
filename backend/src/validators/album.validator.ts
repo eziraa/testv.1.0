6ea188cbb0d366420b591be47c84c20475a0dd65
eziraa <ezratgab@gmail.com>
2025-05-16 21:12:40 +0300

    feat: craete zod validator to validate allbum add and update data

---
+++ backend/src/validators/album.validator.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const AlbumSchemaZod = z.object({
+  title: z.string().min(1, "Title is required"),
+  artist: z.string().regex(/^[a-f\d]{24}$/i, "Invalid artist ID"),
+  releaseDate: z.coerce.date().optional(),
+  coverImage: z.string().url("Invalid URL").optional(),
+});
+
+export const AlbumUpdateSchemaZod = AlbumSchemaZod.partial();
