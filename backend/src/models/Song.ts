89dc027ea210f95b25227d95867071c9a753153c
eziraa <ezratgab@gmail.com>
2025-05-16 15:18:58 +0300

    update: update song model

--- backend/src/models/Song.ts
+++ backend/src/models/Song.ts
@@ -2,18 +2,20 @@ import mongoose, { Document, Schema } from 'mongoose';
   
   export interface ISong extends Document {
     title: string;
-    artist: mongoose.Types.ObjectId;
-    album?: mongoose.Types.ObjectId;
+    artist: mongoose.Types.ObjectId ;
+    album?: mongoose.Types.ObjectId  ;
     genre?: string;
     audioUrl: string;
+    releaseDate?: Date;
   }
   
   const SongSchema = new Schema<ISong>({
     title: { type: String, required: true },
-    artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
+    artist: { type: mongoose.Schema.Types.ObjectId , ref: 'Artist' },
     album: { type: mongoose.Schema.Types.ObjectId, ref: 'Album' },
     genre: String,
-    audioUrl: { type: String, required: true }
+    audioUrl: { type: String, required: true },
+    releaseDate: { type: Date, default: Date.now },
   });
   
   export default mongoose.model<ISong>('Song', SongSchema);
