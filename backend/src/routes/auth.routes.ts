8d7269d3e908fb7df2efbd4b34615fdde3338010
eziraa <ezratgab@gmail.com>
2025-05-17 09:25:04 +0300

    feat: create auth endpoints

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import authController from "../controllers/auth.controller";
+import { AuthenticatedOnly } from "../middleware/auth.middleware";
+
+const router = Router();
+
+// Authentication routes
+router.post("/login", authController.login);
+router.post("/signup", authController.signUp);
+router.post("/update-profile",AuthenticatedOnly, authController.updateUserProfile);
+router.get("/me",AuthenticatedOnly, authController.getMe);
+
+
+export default router;
