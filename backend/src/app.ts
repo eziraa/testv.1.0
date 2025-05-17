4fb2aabefd767c3fa5c91f8ed1ecb7bf8b4b1d1a
eziraa <ezratgab@gmail.com>
2025-05-17 09:25:32 +0300

    code: cleaning

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import songRoutes from './routes/song.routes';
 
+import songRoutes from './routes/song.routes';
+import albumRoutes from './routes/album.routes';
+import artistRoutes from './routes/artist.routes';
+import playlistRoutes from './routes/playlist.routes';
+import authRoutes from './routes/auth.routes';
+
+
+
+declare global {
+    namespace NodeJS {
+        interface Request extends Express.Request{}
+        interface Response extends Express.Response{}
+    }
+}
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 app.use('/api/songs', songRoutes);
+app.use('/api/albums', albumRoutes);
+app.use('/api/artists', artistRoutes);
+app.use('/api/playlists', playlistRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Music API');
+});
+
 
 export default app;
--- backend/src/config/connection.ts
+++ backend/src/config/connection.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import mongoose from "mongoose";
+import listEndpoints from 'express-list-endpoints'
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/test_songs';
 const PORT = process.env.PORT || 5000
@@ -9,6 +10,7 @@ const DBConnection = (app: Express) => {
     mongoose.connect(MONGO_URI)
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        
     })
     .catch(err => console.error(err));
 }
