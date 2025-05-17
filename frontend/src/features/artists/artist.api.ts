1fec5e7fefa78a3ab2ed99737a3c2cc214c46e87
eziraa <ezratgab@gmail.com>
2025-05-17 18:43:42 +0300

    feat: create rartist apit service methods

---
+++ frontend/src/features/artists/artist.api.ts
@@ -0,0 +1,15 @@
+import axiosClient from '../config/axiosClient';
+import type { ArtistPayload } from './artist.types';
+
+
+const API_BASE = `${import.meta.env.VITE_API_BASE_URL}/artists`;
+
+export const artistAPI = {
+  fetchArtists: () => axiosClient.get(API_BASE),
+  createArtist: (data: ArtistPayload) => axiosClient.post(API_BASE, data),
+  updateArtist: (id: string, data: Partial<ArtistPayload>) => axiosClient.put(`${API_BASE}/${id}`, data),
+  deleteArtist: (id: string) => axiosClient.delete(`${API_BASE}/${id}`),
+  getArtistById: (id: string) => axiosClient.get(`${API_BASE}/${id}`),
+};
+
+
