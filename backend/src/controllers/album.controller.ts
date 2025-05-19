5677748d4d4cad49f047769019620c5eb59c139c
eziraa <ezratgab@gmail.com>
2025-05-19 10:19:49 +0300

    update: album adding and editing controller to able to upload cover image

--- backend/src/controllers/album.controller.ts
+++ backend/src/controllers/album.controller.ts
@@ -1,68 +1,123 @@
-import { Request, Response } from 'express';
-import Album from '../models/Album';
-import { AlbumSchemaZod, AlbumUpdateSchemaZod } from '../validators/album.validator';
-
+import { Request, Response } from "express";
+import Album from "../models/Album";
+import {
+  AlbumSchemaZod,
+  AlbumUpdateSchemaZod,
+} from "../validators/album.validator";
+import fileUploader from "../utils/FileUploader";
 class AlbumController {
-
   // METHOD: To get all albums
   async getAllAlbums(_: Request, res: Response) {
-    const albums = await Album.find().populate('artist');
+    const albums = await Album.find().populate("artist");
     res.json(albums);
   }
 
   // METHOD: to create new album
   async createAlbum(req: Request, res: Response) {
     try {
-      const parsed = AlbumSchemaZod.parse(req.body);
+      let coverImageUrl: string | undefined;
+
+      // If image was uploaded, get its URL
+      if (req.file) {
+        coverImageUrl = fileUploader.getFileUrl(req, req.file.filename);
+      }
+
+      // Parse songs from JSON string to array
+      if (!req.body.songs) {
+        req.body.songs = [];
+      } else {
+        req.body.songs = JSON.parse(req.body.songs);
+      }
+      const inputData = {
+        ...req.body,
+        coverImage: coverImageUrl ?? req.body.coverImage,
+      };
+
+      const parsed = AlbumSchemaZod.parse(inputData);
       const album = new Album(parsed);
       await album.save();
       res.status(201).json(album);
     } catch (err) {
-      if (err instanceof Error && 'errors' in err) {
-         res.status(400).json({
-          message: 'Validation failed',
+      if (err instanceof Error && "errors" in err) {
+        res.status(400).json({
+          message: "Validation failed",
           errors: (err as any).errors,
         });
       }
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({ message: "Internal Server Error" });
     }
   }
 
   // METHOD: to get album by ID
   async getAlbumById(req: Request, res: Response) {
     try {
-      const album = await Album.findById(req.params.id).populate('artist');
-      if (!album) 
-         res.status(404).json({ message: 'Album not found' });
+      const album = await Album.findById(req.params.id).populate("artist");
+      if (!album) res.status(404).json({ message: "Album not found" });
       res.json(album);
     } catch {
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({ message: "Internal Server Error" });
     }
   }
 
   // METHOD: to update album
   async updateAlbum(req: Request, res: Response) {
     try {
-      const parsed = AlbumUpdateSchemaZod.parse(req.body);
-      const album = await Album.findByIdAndUpdate(req.params.id, parsed, { new: true });
-      if (!album)  
-        res.status(404).json({ message: 'Album not found' });
+      let coverImageUrl: string | undefined;
+      // If image was uploaded, get its URL
+      if (req.file) {
+        coverImageUrl = fileUploader.getFileUrl(req, req.file.filename);
+      }
+      // Parse songs from JSON string to array
+      if (!req.body.songs) {
+        req.body.songs = [];
+      } else {
+        req.body.songs = JSON.parse(req.body.songs);
+      }
+      const inputData = {
+        ...req.body,
+        coverImage: coverImageUrl ?? req.body.coverImage,
+      };
+
+      const parsed = AlbumUpdateSchemaZod.parse(inputData);
+      // Delete the coverImage property if it is not provided
+      if (!coverImageUrl) {
+        const album = await Album.findById(req.params.id);
+        if (album?.coverImage)
+          fileUploader.deleteFile(album.coverImage);
+      }
+      const album = await Album.findByIdAndUpdate(req.params.id, parsed, {
+        new: true,
+      });
+      if (!album) res.status(404).json({ message: "Album not found" });
       res.json(album);
     } catch (err) {
-      if (err instanceof Error && 'errors' in err) {
-         res.status(400).json({
-          message: 'Validation failed',
+      if (err instanceof Error && "errors" in err) {
+        res.status(400).json({
+          message: "Validation failed",
           errors: (err as any).errors,
         });
       }
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({ message: "Internal Server Error" });
     }
   }
 
   // DELETE /albums/:id
   async deleteAlbum(req: Request, res: Response) {
-    await Album.findByIdAndDelete(req.params.id);
-    res.status(204).send();
+    
+    try {
+      const album = await Album.findById(req.params.id)  
+      
+      if(!album)
+        res.status(404).json({message: "Album not found with this ID"})
+      else{
+        if(album.coverImage)
+          fileUploader.deleteFile(album.coverImage)
+      }
+      await Album.findByIdAndDelete(req.params.id);
+      res.status(204).send();
+    } catch (error) {
+      
+    }
   }
 }
 
--- backend/src/routes/album.routes.ts
+++ backend/src/routes/album.routes.ts
@@ -1,12 +1,21 @@
-import { Router } from 'express';
-import albumController from '../controllers/album.controller';
+import { Router } from "express";
+import albumController from "../controllers/album.controller";
+import fileUpLoader from "../utils/FileUploader";
 
 const router = Router();
+const uploader = fileUpLoader.getUploader();
 
-router.get('/', albumController.getAllAlbums);
-router.post('/', albumController.createAlbum);
-router.get('/:id', albumController.getAlbumById);
-router.put('/:id', albumController.updateAlbum);
-router.delete('/:id', albumController.deleteAlbum);
+router.get("/", albumController.getAllAlbums);
+router.post(
+  "/",
+  uploader.single('coverImage'),
+  albumController.createAlbum
+);
+router.get("/:id", albumController.getAlbumById);
+router.put("/:id",
+  uploader.single('coverImage'),
+   albumController.updateAlbum,
+  );
+router.delete("/:id", albumController.deleteAlbum);
 
 export default router;
